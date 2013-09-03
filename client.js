3c3ede3a6abcfcb3f0f9cea9471a849cfb081d63
Thabo <tha.bo.fletcher@gmail.com>
2013-09-02 23:17:13 -0700

    zend xmlrequest mock

--- client.js
+++ client.js
@@ -1,22 +1,34 @@
-var request = require('request');
-var cheerio = require('cheerio');
+var http = require('http');
 
 exports.query = function(request, callback) {
-	callback(queryById(request.url.substr(1)));
-}
+	var post_data = 'ClientCandidateID=' + request.url.substr(1) + '&_=';
 
-var queryById = function(id, callback) {
-	var url = '';
-	request.get(url, function(err, resp, body) {
-	    if (err)
-	        throw err;
-	    if (resp.statusCode == 200) {
-	    	$ = cheerio.load(body);
-		    
-	    	// parse here and fill out returnedObj
+	// An object of options to indicate where to post to
+	var post_options = {
+	    host: 'www.zend.com',
+	    port: '80',
+	    path: '/en/store/education/certification/yellow-pages.php',
+	    method: 'POST',
+	    headers: {
+			//'Origin': 'http://www.zend.com',
+			'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+			'Accept': 'text/javascript, text/html, application/xml, text/xml, */*',
+			//'X-Prototype-Version': '1.5.1.1',
+			//'X-Return-Type': 'JSON',
+			'X-Requested-With': 'XMLHttpRequest',
+			//'Referer': 'http://www.zend.com/store/education/certification/yellow-pages.php',
+			//'Accept-Encoding': 'gzip,deflate,sdch',
+			//'Accept-Language': 'en-US,en;q=0.8'
+			}
+	};
 
-			var returnedObj = {};
-			callback(JSON.stringify(returnedObj));
-		}
-	});	
+	var post_req = http.request(post_options, function(res) {
+		res.setEncoding('utf8');
+	    res.on('data', function(json) {
+	    	callback(json);
+	    });
+	});
+
+	post_req.write(post_data);
+	post_req.end();
 }
---
+++ query.js
@@ -0,0 +1,32 @@
+var http = require('http');
+
+var post_data = 'ClientCandidateID=ZEND003504&_=';
+
+// An object of options to indicate where to post to
+var post_options = {
+    host: 'www.zend.com',
+    port: '80',
+    path: '/en/store/education/certification/yellow-pages.php',
+    method: 'POST',
+    headers: {
+		//'Origin': 'http://www.zend.com',
+		'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+		'Accept': 'text/javascript, text/html, application/xml, text/xml, */*',
+		//'X-Prototype-Version': '1.5.1.1',
+		//'X-Return-Type': 'JSON',
+		'X-Requested-With': 'XMLHttpRequest',
+		//'Referer': 'http://www.zend.com/store/education/certification/yellow-pages.php',
+		//'Accept-Encoding': 'gzip,deflate,sdch',
+		//'Accept-Language': 'en-US,en;q=0.8'
+		}
+};
+
+var post_req = http.request(post_options, function(res) {
+	res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log('Response: ' + chunk);
+    });
+});
+
+post_req.write(post_data);
+post_req.end();
